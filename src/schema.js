6083dc2b335ec50d9ef0f50cf655e203c4862643
Wryhder <jemimahsummers@yahoo.com>
2020-02-08 16:46:05 +0100

    Modify GraphQL schema

--- src/schema.js
+++ src/schema.js
@@ -1,8 +1,23 @@
 const { buildSchema } = require("graphql");
 
 const schema = buildSchema(`
+  type Event {
+    id: ID!
+    title: String
+    description: String
+    date: String
+    attendants: [Person!]
+  }
+
+  type Person {
+    id: ID!
+    name: String!
+    age: Int
+  }
+
   type Query {
-    hello: String
+    events: [Event!]!
+    event(id: Int!): Event!
   }
 `);
 
