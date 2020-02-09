a8928f72d190f0b86119a4e1d59a0cd9c9672e69
Wryhder <jemimahsummers@yahoo.com>
2020-02-09 14:49:47 +0100

    Provide visual interface (GraphQL Playground) to query server

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const graphqlHTTP = require("express-graphql");
 const schema = require("./schema");
 const startDatabase = require("./database");
+const expressPlayground = require("graphql-playground-middleware-express").default;
 
 const context = async () => {
   const db = await startDatabase();
@@ -38,6 +39,7 @@ app.use(
   })
 );
 
+app.get("/playground", expressPlayground({ endpoint: "/graphql" }));
 app.listen(4000);
 
 console.log(`🚀 Server ready at http://localhost:4000/graphql`);
